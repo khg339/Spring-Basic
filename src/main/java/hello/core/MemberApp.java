b21aff2028bc73bc846d035488b6d4fbeb8dcd99
khg339 <khg33333339@gmail.com>
2022-01-12 18:22:26 +0900

    예제만들기 only JAVA - 회원서비스 작동

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl(); //회원 서비스
+        Member member = new Member(1L, "memberA", Grade.Vip); //회원(아이디 1, 이름 memberA, 등급 vip)
+        memberService.join(member); //회원 가입
+
+        Member findMember = memberService.findMember(1L); //찾을 회원(아이디 1)
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
